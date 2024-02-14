06024eaf37775793bad1257e82064142110a5b7e
nematatu <nematatu1003@gmail.com>
2024-02-15 03:07:22 +0900

    hello

--- src/app.py
+++ src/app.py
@@ -1,1 +1,11 @@
-print("hello, world!!!!!!!!!!!!!!!!!!!!")
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_ky=os.environ['OPENAI_API_KEY']
+
+client=OpenAI(api_key=api_ky)
+
+print("hello")
