61415bf89b1ceeadc94c9334abde7be7f29fd3d1
nematatu <nematatu1003@gmail.com>
2024-02-15 19:51:05 +0900

    blackでフォーマット

--- src/app.py
+++ src/app.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api_ky=os.environ['OPENAI_API_KEY']
+api_ky = os.environ["OPENAI_API_KEY"]
 
-client=OpenAI(api_key=api_ky)
+client = OpenAI(api_key=api_ky)
 
 print("hello")
