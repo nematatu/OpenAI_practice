25708d16f133513f24ae99274ba96dc38cddb2f0
nematatu <nematatu1003@gmail.com>
2024-02-16 14:06:14 +0900

    flaskの追加

--- src/app.py
+++ src/app.py
@@ -1,11 +1,19 @@
 from openai import OpenAI
+import sptipy
+from spotipy.oauth2 import SpotifyClientCredentials
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
 api_ky = os.environ["OPENAI_API_KEY"]
+client_id = os.environ.get("SP_CLI_KEY") 
+client_secret = os.environ.get("SP_SCR_KEY")
+
+client_credentials_manager = spotipy.oauth2.SpotifyClientCredentials(client_id, client_secret)
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 client = OpenAI(api_key=api_ky)
 
 print("hello")
+
