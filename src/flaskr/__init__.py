5f3661e014d057a1fae737a40ffe3b26040ee281
nematatu <nematatu1003@gmail.com>
2024-02-18 23:30:48 +0900

    yaa

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -17,11 +17,14 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route("/hello")
+    @app.route("/")
     def hello():
         return "Hello, World!"
     
     from . import db
     db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
 
     return app
--- src/flaskr/app.py
+++ src/flaskr/app.py
@@ -1,7 +1,5 @@
-from flask import Flask
+from flaskr import create_app
 
-app=Flask(__name__)
+app=create_app()
 
-@app.route("/")
-def hello():
-    return 'Hello, World!'
+app.run()
