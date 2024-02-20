6a893ee57172ef1bc7263f40cb26955c72030ba5
nematatu <nematatu1003@gmail.com>
2024-02-20 21:36:34 +0900

    完成！

--- src/flaskr/__init__.py
+++ src/flaskr/__init__.py
@@ -16,10 +16,6 @@ def create_app(test_config=None):
         os.mkdir(app.instance_path)
     except OSError:
         pass
-
-    @app.route("/")
-    def hello():
-        return "Hello, World!"
     
     from . import db
     db.init_app(app)
