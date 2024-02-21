a7900c93f958253c68398104760fce15c5b78e67
nematatu <nematatu1003@gmail.com>
2024-02-21 18:33:15 +0900

    twitterOAuth

--- src/flaskr/auth.py
+++ src/flaskr/auth.py
@@ -16,29 +16,84 @@ import urllib.parse as parse
 from dotenv import load_dotenv
 import os
 
+load_dotenv()
 api_key = os.environ["TW_CLI_KEY"]
 api_secret = os.environ["TW_SCR_KEY"]
+
 # Twitter Endpoint
-twitter_base_url = 'https://api.twitter.com'
-authorization_endpoint = twitter_base_url + '/oauth/authenticate'
-request_token_endpoint = twitter_base_url + '/oauth/request_token'
-token_endpoint = twitter_base_url + '/oauth/access_token'
-credentials=twitter_base_url + '/1.1/account/verify_credentials.json'
+twitter_base_url = "https://api.twitter.com"
+authorization_endpoint = twitter_base_url + "/oauth/authenticate"
+request_token_endpoint = twitter_base_url + "/oauth/request_token"
+token_endpoint = twitter_base_url + "/oauth/access_token"
+credentials = twitter_base_url + "/1.1/account/verify_credentials.json"
 
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
-@bp.route("/twitter_login",methods=("GET","POST"))
+
+@bp.route("/twitter_login", methods=("GET", "POST"))
 def twitter_login():
-    return "hello"
+    # 1.リクエストトークンを取得する。
+    # (Step 1: Obtaining a request token:https://developer.twitter.com/en/docs/authentication/guides/log-in-with-twitter)
+    twitter = OAuth1Session(api_key, api_secret)
+    # oauth_callback = request.args.get('oauth_callback')
+    oauth_callback = "http://0.0.0.0:5005/auth/callback"
+    res = twitter.post(
+        request_token_endpoint, params={"oauth_callback": oauth_callback}
+    )
+    request_token = dict(parse.parse_qsl(res.content.decode("utf-8")))
+    print(request_token)
+    oauth_token = request_token["oauth_token"]
+    oauth_token_secret = request_token["oauth_token_secret"]
+    # 2.リクエストトークンを指定してTwitterへ認可リクエスト(Authorization Request)を行う。
+    # (Step 2: Redirecting the user:https://developer.twitter.com/en/docs/authentication/guides/log-in-with-twitter#tab2)
+    return redirect(
+        authorization_endpoint
+        + "?{}".format(parse.urlencode({"oauth_token": oauth_token}))
+    )
+
+
+@bp.route("/callback")
+def callback():
+    # 3.ユーザー認証/同意を行い、認可レスポンスを受け取る。
+    oauth_verifier = request.args.get("oauth_verifier")
+    oauth_token = request.args.get("oauth_token")
+
+    # 4.認可レスポンスを使ってトークンリクエストを行う。
+    # (Step 3: Converting the request token to an access token:https://developer.twitter.com/en/docs/authentication/guides/log-in-with-twitter#tab3)
+    twitter = OAuth1Session(api_key, api_secret, oauth_token)
+
+    res = twitter.post(token_endpoint, params={"oauth_verifier": oauth_verifier})
+
+    access_token = dict(parse.parse_qsl(res.content.decode("utf-8")))
+
+    twitter = OAuth1Session(
+        api_key,
+        api_secret,
+        access_token["oauth_token"],
+        access_token["oauth_token_secret"],
+    )
+    response = twitter.get(
+        "https://api.twitter.com/1.1/account/verify_credentials.json"
+    )
+    user_info = response.json()
+
+    # ユーザー名とアイコンのURLを取得
+    username = user_info["screen_name"]
+    icon_url = user_info["profile_image_url_https"]
+    name = user_info["name"]
+
+    # return jsonify({'username':username,'icon_url':icon_url,'name':name})
+    return redirect(url_for("index"))
+
     # if not resp.ok:
     #     msg="Failed to fetch user info."+provider_name,
     #     flash(msg,category="error")
     #     return False
-    
+
     # if blueprint is twitter_bp:
     #     provider_user_id=resp.json()["id_str"]
     #     provider_user_name=resp.json()["screen_name"]
-    
+
     # select_oauth=(
     #     f"SELECT * FROM oauth WHERE"
     #     f"provider='{provider_name}' and"
@@ -66,11 +121,6 @@ def twitter_login():
     #     msg="Logged in successfully."+provider_name
     #     flash(msg)
     #     return False
-
-
-
-
-
 
 
 @bp.route("/register", methods=("GET", "POST"))
